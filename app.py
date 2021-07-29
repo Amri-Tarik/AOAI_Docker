b153836bcbf950a85fe8aab611fcf58e2d4f4ef2
Tarik-Amri <amri.tarik0@gmail.com>
2021-07-29 19:08:10 +0100

    switched to mongodb

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
-import pandas as pd
+
 import numpy as np
 import sys
+import pymongo
 from pickled import *
 
 pca = unpickled("pca")
@@ -8,12 +9,11 @@ clf_elec = unpickled("clf_elec")
 clf_indus = unpickled("clf_indus")
 clf_meca = unpickled("clf_meca")
 
-#replace with mongodb grabber /TODO
-data = pd.read_csv('cleaned.csv')
-data.drop(["Unnamed: 0","FILIERE","OUTPUT"],inplace=True,axis=1)
-liste = data.iloc[int(sys.argv[1]),:]
-liste = liste.values.reshape(1, -1)
-###################################
+client = pymongo.MongoClient("mongodb+srv://tfa7a:tfa7a123@aoai-cluster.tzpfo.mongodb.net/myFirstDatabase?retryWrites=true&w=majority")
+db = client.students
+
+notes = db.aoai.find_one({ "id" : str(sys.argv[1]) })["notes"]
+liste = np.array([float(note) for note in notes]).reshape(1, -1)
 
 X = pca.transform(liste)
 print('{ "gi" : "' + str(float("{:.2f}".format(clf_indus.predict(X)[0]* 100))) + '", "gm" : "' + str(float("{:.2f}".format(clf_meca.predict(X)[0]* 100))) + '" , "ge" : "' + str(float("{:.2f}".format(clf_elec.predict(X)[0]* 100))) + '" }')
